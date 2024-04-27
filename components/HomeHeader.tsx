fbb28b1dc9179c558ebe7dfe48a4e86d8009c042
DaysonTai1848 <daysontai69@gmail.com>
2024-04-27 16:21:32 +0800

    logo

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { SiAlibabadotcom } from "react-icons/si";
+import Link from "next/link";
+import { Bold } from "lucide-react";
 
 const HomeHeader = () => {
   return (
-    <div className="w-screen flex flex-col">
-      <div className="px-6 py-3 flex justify-between">
+    <div className="w-screen flex flex-col bg-black">
+      <div className="px-10 py-2 flex justify-between">
         <div>
-          <SiAlibabadotcom size={50} />
+          <Link href="/">
+            <div className="flex flex-row justify-center items-center space-x-1">
+              <SiAlibabadotcom size={55} color="white" />
+              <span className="text-white text-2xl">
+                <span className="font-bold">Alibaba</span>.com
+              </span>
+            </div>
+          </Link>
         </div>
         <div className="flex flex-row space-x-6">
           <div>Deliver to</div>
